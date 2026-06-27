bec4590634574f948cfd09bfcf3a053de6329e04
Sakshat Kumar <sakshat@settlemintindia.com>
2026-06-28 00:05:54 +0530

    feat: implement UserStorage module and refactor AuthStorage lifecycle methods

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,8 @@
+export { AuthStorage } from "./auth-storage";
+export type { AuthResponse, UserResponse } from "./auth-storage";
+export { TokenStorage } from "./token-storage";
+export type { TokenData } from "./token-storage";
+export { UserStorage } from "./user-storage";
+export * from "./current-user";
+export { AUTH_STORAGE_KEYS } from "./auth-constants";
+export type { AuthStorageKeys } from "./auth-constants";
